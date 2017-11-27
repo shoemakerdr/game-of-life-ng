76dcd8bf32cfd15bba0f6cac700a827fdba29d54
Derek Shoemaker <shoemakerdr@gmail.com>
2017-11-26 19:58:12 -0600

    2 spaces -> 4 spaces

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { GameService } from './services/game.service';
 
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [GameService],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
