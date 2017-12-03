e5b4eb0edde6894b0bbe334d1203e8bb8510b3a2
Derek Shoemaker <shoemakerdr@gmail.com>
2017-12-02 23:58:39 -0600

    added tests for toggleLife method

--- src/app/services/tests/cell.spec.ts
+++ src/app/services/tests/cell.spec.ts
@@ -40,3 +40,20 @@ describe('nextGenerationCell method', () => {
         })
     })
 })
+
+describe('toggleLife method', () => {
+    describe('when called on a dead cell', () => {
+        const cell = new Cell(false)
+        it('should make the cell alive', () => {
+            cell.toggleLife()
+            expect(cell.getIsLiving()).toEqual(true)
+        })
+    })
+    describe('when called on a living cell', () => {
+        const cell = new Cell(true)
+        it('should make the cell dead', () => {
+            cell.toggleLife()
+            expect(cell.getIsLiving()).toEqual(false)
+        })
+    })
+})
