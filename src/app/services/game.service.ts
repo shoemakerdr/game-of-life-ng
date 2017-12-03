93acb72691f35739e2ef29c0ccc90aaa8f857a35
Derek Shoemaker <shoemakerdr@gmail.com>
2017-12-03 00:00:44 -0600

    Added getGrid, setGrid, getGameState, setNextGameState, and newGrid methods.

--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core'
 import { Cell } from './cell'
+import { Grid } from './grid'
+import { Location } from './location'
 
 @Injectable()
 export class GameService {
-
+    grid:Grid
     constructor () {}
 
+    getGrid () : Grid {
+        return this.grid
+    }
+
+    setGrid (grid:Grid) : void {
+        this.grid = grid
+    }
+
+    getGameState () : Cell[][] {
+        return this.grid.getState()
+    }
+
+    setNextGameState () : void {
+        this.grid.setState(this.grid.nextState())
+    }
+
+    newGrid (rowLength:number, columnLength:number) : Grid {
+        const grid = []
+        for (let i = 0; i < columnLength; i++) {
+            const row = []
+            for (let j = 0; j < rowLength; j++) {
+                row.push(new Cell(false))
+            }
+            grid.push(row)
+        }
+        return new Grid(grid)
+    }
+
+    toggleCellAt (location:Location) : void {
+        this.grid.toggleCellLife(location)
+    }
 }
