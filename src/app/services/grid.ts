b341e87d75aa2cb1a9022eef664a9e52fcda64a0
Derek Shoemaker <shoemakerdr@gmail.com>
2017-12-02 22:38:21 -0600

    Added isValidLocation, isCellLiving, getState, and toggleCell methods. Implemented getLivingNeighbors and getNextState methods.

--- src/app/services/grid.ts
+++ src/app/services/grid.ts
@@ -15,30 +15,40 @@ export class Grid {
         }
     }
 
+    getState () : Cell[][] {
+        return this.state
+    }
+
     nextState () : Cell[][] {
-        // TODO: implement nextState
-        const next = [[]]
-        return next
-    }
-
-    getNeighbors (location:Location) : number {
-        // TODO: implement getNeighbors
-        const surrounding = this.filterSurroundingByBounds(this.getSurrounding(location))
-        return 0
-    }
-
-    filterSurroundingByBounds (surrounding:Location[]) : Location[] {
-        const { rowLower, rowUpper, columnLower, columnUpper } = this.bounds
-        return surrounding.filter(location => {
-            const { row, column } = location
-            return row >= rowLower && row <= rowUpper
-                && column >= columnLower && column <= columnUpper
+        return this.getState().map((row, r) => {
+            return row.map((column, c) => {
+                const neighbors = this.getLivingNeighbors({row: r, column: c})
+                return column.nextGenerationCell(neighbors)
+            })
         })
+    }
+
+    toggleCell (location:Location) : void {
+        const { row, column } = location
+        const cell = this.state[row][column]
+        this.state[row][column] = new Cell(!cell.getIsLiving())
+    }
+
+    getLivingNeighbors (location:Location) : number {
+        const surrounding = this.filterSurrounding(this.getSurrounding(location))
+        const neighbors = surrounding.filter(location => this.isCellLiving(location))
+        return neighbors.length
+    }
+
+    filterSurrounding (surrounding:Location[]) : Location[] {
+        return surrounding.filter(location => this.isValidLocation(location))
     }
 
     getSurrounding (location:Location) : Location [] {
         const { row, column } = location
-        const surrounding = [
+        if (!this.isValidLocation(location))
+            return []
+        return [
             { row: row, column: column - 1 },
             { row: row + 1, column: column - 1 },
             { row: row + 1, column: column},
@@ -48,6 +58,17 @@ export class Grid {
             { row: row - 1, column: column },
             { row: row - 1, column: column - 1 },
         ]
-        return surrounding
+    }
+
+    isValidLocation (location:Location) : boolean {
+        const { rowLower, rowUpper, columnLower, columnUpper } = this.bounds
+        const { row, column } = location
+        return row >= rowLower && row < rowUpper
+            && column >= columnLower && column < columnUpper
+    }
+
+    isCellLiving (location:Location) : boolean {
+        const { row, column } = location
+        return this.getState()[row][column].getIsLiving()
     }
 }
